e195eac5b54a4ba54a5a211843898becc9a2f35c
yuyoyanaoya <csanadvass496@gmail.com>
2026-09-16 17:47:51 +0200

    Add files via upload

---
+++ fórum projekt/index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Squall ・ Lurk more - it's never enough</title>
+    <link rel="icon" href="icon2.ico">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Imbue:opsz,wght@10..100,100..900&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Habibi&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Habibi&family=Sansation:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+</head>
+
+<style>
+    body {
+        background-image: url("mainBackground.jpg");
+        background-repeat: no-repeat;
+	background-size: cover;
+	background-position: center;
+    }
+
+    th, td {
+        width:409px;
+        height: fit-content;
+        font-family: Habibi;
+        color: white;
+    }
+
+    a:link, a:visited, a:hover, a:active {
+        text-decoration: none;
+        color: white;
+    }
+
+    a:hover {
+        text-decoration: underline;
+        color: rgb(236, 236, 236);
+    }
+
+    td {
+        padding-top: 10px;
+        padding-right: 10px;
+        padding-left: 10px;
+    }
+
+
+    #kategoriak {
+        text-align: justify;
+        border: 1px black solid;
+        width: 1257px;
+        height: fit-content;
+        margin-left: auto;
+        margin-right: auto;
+        padding: 20px;
+        background-color: rgba(51, 51, 51, 0.456);
+    }
+
+    #titleDIV {
+        font-family: DM Serif Display;
+        text-align: center;
+        padding-bottom: 50px;
+        color: white;
+    }
+
+    #title {
+        font-size: 120px;
+    }
+
+    #subtitle {
+        text-decoration: none;
+    }
+
+    h2 {
+        margin: 0;
+    }
+
+    .felsoSor {
+        margin-top: 0;
+    }
+
+    hr {
+        width: 310%;
+    }
+
+</style>
+
+<body>
+    <div id="titleDIV">
+        <p id="title">Squall</p>
+        <h2 id="subtitle">Lurk more - <i>it's never enough</i></h2>
+    </div>
+
+    <div id="kategoriak">
+        <table>
+            <tr>
+                <th><h2 class="felsoSor">Zene</h2></th>
+                <th><h2 class="felsoSor">Tech</h2></th>
+                <th><h2 class="felsoSor">Gasztronómia</h2></th>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr><td><hr></td></tr>
+            <tr>
+                <th><h2>Kábítószerek</h2></th>
+                <th><h2>Pörköltfesztivál</h2></th>
+                <th><h2>Persona 4 Golden</h2></th>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr><td><hr></td></tr>
+            <tr>
+                <th><h2 class="felsoSor">Zene</h2></th>
+                <th><h2 class="felsoSor">Tech</h2></th>
+                <th><h2 class="felsoSor">Gasztronómia</h2></th>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr><td><hr></td></tr>
+            <tr>
+                <th><h2>Kábítószerek</h2></th>
+                <th><h2>Pörköltfesztivál</h2></th>
+                <th><h2>Persona 4 Golden</h2></th>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr><td><hr></td></tr>
+            <tr>
+                <th><h2 class="felsoSor">Zene</h2></th>
+                <th><h2 class="felsoSor">Tech</h2></th>
+                <th><h2 class="felsoSor">Gasztronómia</h2></th>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr><td><hr></td></tr>
+            <tr>
+                <th><h2>Kábítószerek</h2></th>
+                <th><h2>Pörköltfesztivál</h2></th>
+                <th><h2>Persona 4 Golden</h2></th>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr><td><hr></td></tr>
+            <tr>
+                <th><h2 class="felsoSor">Zene</h2></th>
+                <th><h2 class="felsoSor">Tech</h2></th>
+                <th><h2 class="felsoSor">Gasztronómia</h2></th>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+            <tr><td><hr></td></tr>
+            <tr>
+                <th><h2>Kábítószerek</h2></th>
+                <th><h2>Pörköltfesztivál</h2></th>
+                <th><h2>Persona 4 Golden</h2></th>
+            </tr>
+            <tr>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+                <td><a href="#">Lorem</a href="#">, <a href="#">ipsum</a>, <a href="#">dolor</a>, <a href="#">sit</a>, <a href="#">amet</a>, <a href="#">consectetur</a>, <a href="#">adipisicing</a>, <a href="#">elit</a></td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
