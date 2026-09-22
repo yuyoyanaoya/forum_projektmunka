ce176dbcedd272fc4a5b4fef7e5404db77475631
yuyoyanaoya <csanadvass496@gmail.com>
2026-09-22 21:13:11 +0200

    Add files via upload

---
+++ index/navbar/rules.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Squall: Szabályzat és általános tudnivalók</title>
+    <link rel="stylesheet" href="rules.css">
+    <link rel="icon" href="../../assets/icon2.ico">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Imbue:opsz,wght@10..100,100..900&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Habibi&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Habibi&family=Sansation:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
+</head>
+
+<script src="../index.js" defer></script>
+
+<body onload="idoMutat()">
+    <div id="balDIV">
+        <div id="balraClock">
+            <div id="clockDIV"></div>
+        </div>
+
+    
+        <div id="titleDIV">
+            <p id="title">Squall</p>
+            <h2 id="subtitle">Lurk more - <i>it's never enough</i></h2>
+        </div>
+    </div>
+
+    <div id="szabalyok">
+        <h1 id="szabalyTitle">Squall fórumszabályzat és általános tudnivalók</h1>
+
+        <p>軋んだ想いを吐き出したいのは存在の証明 が他にないから 掴んだはずの僕の未来は「尊厳」と「自由」で矛盾してるよ 歪んだ残像を消し去りたいのは自分の限界をそこに見るから 自意識過剰な僕の窓には去年のカレンダー日付けがないよ<br>
+
+<h1 id="szabalyTitle">消してリライトして くだらない超幻想 忘られぬ存在感を</h1>
+起死回生 リライトして 意味のない想像も君を成す原動力 全身全霊をくれよ<br> 芽生えてた感情切って泣いて 所詮ただ凡席知って泣いて
+
+<h1 id="szabalyTitle">Kiste, riraotoste kudaranaicsogensó</h1>
+
+
+<br>
+腐った心 を<br>
+薄汚 い嘘を<br>
+消してリライトして<br>
+くだらない超幻想<br>
+忘られぬ存在感を<br>
+<br>
+起死回生<br>
+リライトして<br>
+意味のない想像も君を成す原動力<br>
+全身全霊をくれよ</p>
+    </div>
+
+    <!--<div id="navbar">
+        <table>
+            <tr>
+                <th class="navbarLINK"><a href="#">Hirohito</a></th>
+                <th class="navbarLINK"><a href="../rules.php">Szabályzat</a></th>
+                <th class="navbarLINK"><a href="#">Lorem</a></th>
+                <th class="navbarLINK"><a href="#">Ipsum</a></th>
+                <th class="navbarLINK"><a href="#">Dolor</a></th>
+                <th class="navbarLINK"><a href="#">Sit</a></th>
+            </tr>
+        </table>
+    </div>-->
+
+    
+</body>
+</html>
